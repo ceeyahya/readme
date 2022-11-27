d0950cc039dc6ad15a6ad8d0b500a8222ec3ff91
Yahya <chahineyahya@pm.me>
2022-11-27 07:03:53 +0000

    Updated the header badges

--- content.js
+++ content.js
@@ -1,7 +1,12 @@
+const header = `
+[![Mastodon](https://img.shields.io/badge/Mastodon-5c4bdf?style=for-the-badge&logo=Mastodon&logoColor=ffffff&link=https://fosstodon.org/@hya)](https://fosstodon.org/@hya)
+[![LinkedIn](https://img.shields.io/badge/LinkedIn-0A66C2?style=for-the-badge&logo=LinkedIn&logoColor=ffffff&link=https://www.linkedin.com/in/yahya-chahine/)](https://www.linkedin.com/in/yahya-chahine/)
+[![Portfolio](https://img.shields.io/badge/Portfolio-6366f1?style=for-the-badge&logo=Internet-Explorer&logoColor=ffffff&link=https://www.chahineyahya.dev)](https://www.chahineyahya.dev)
+[![Check out the Live Version](https://img.shields.io/badge/Check%20out%20the%20Live%20Version-6366f1?style=for-the-badge&logoColor=ffffff&link=https://ADD-PROJECT-LINK-HERE)](https://ADD-PROJECT-LINK-HERE)
+`;
+
 const fsw = `
-![Mastodon Follow](https://img.shields.io/mastodon/follow/109264261075497898?domain=https%3A%2F%2Ffosstodon.org&label=Mastodon&style=social)
-[![Linkedin](https://img.shields.io/badge/LinkedIn-%230077B5.svg?&style=flat-square&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/yahya-chahine/)
-[![Portfolio](https://img.shields.io/badge/Portfolio-Visit%20my%20Website-green.svg)](https://chahineyahya.dev)
+${header}
 # ADD THE MISSING BADGES
 # Title
 <hr></hr>\n
@@ -13,9 +18,7 @@ const fsw = `
 `;
 
 const few = `
-![Mastodon Follow](https://img.shields.io/mastodon/follow/109264261075497898?domain=https%3A%2F%2Ffosstodon.org&label=Mastodon&style=social)
-[![Linkedin](https://img.shields.io/badge/LinkedIn-%230077B5.svg?&style=flat-square&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/yahya-chahine/)
-[![Portfolio](https://img.shields.io/badge/Portfolio-Visit%20my%20Website-green.svg)](https://chahineyahya.dev)
+${header}
 # ADD THE MISSING BADGES
 # Title
 <hr></hr>\n
@@ -25,9 +28,7 @@ const few = `
 `;
 
 const dsw = `
-![Mastodon Follow](https://img.shields.io/mastodon/follow/109264261075497898?domain=https%3A%2F%2Ffosstodon.org&label=Mastodon&style=social)
-[![Linkedin](https://img.shields.io/badge/LinkedIn-%230077B5.svg?&style=flat-square&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/yahya-chahine/)
-[![Portfolio](https://img.shields.io/badge/Portfolio-Visit%20my%20Website-green.svg)](https://chahineyahya.dev)
+${header}
 # ADD THE MISSING BADGES
 # Title
 <hr></hr>\n
@@ -40,9 +41,7 @@ const dsw = `
 `;
 
 const ds = `
-![Mastodon Follow](https://img.shields.io/mastodon/follow/109264261075497898?domain=https%3A%2F%2Ffosstodon.org&label=Mastodon&style=social)
-[![Linkedin](https://img.shields.io/badge/LinkedIn-%230077B5.svg?&style=flat-square&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/yahya-chahine/)
-[![Portfolio](https://img.shields.io/badge/Portfolio-Visit%20my%20Website-green.svg)](https://chahineyahya.dev)
+${header}
 # ADD THE MISSING BADGES
 <hr></hr>\n
 # Title
